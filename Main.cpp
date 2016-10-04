258727fcebee15f2af988016e3c4818fc0f7c17f
Marcus Mirzaie <XMBMirzaie@gmail.com>
2016-10-04 09:26:28 -0500

    added argument handlers and finalized CLI version

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+
 using namespace std;
-int main() {
-	cout << "Hello, world" << endl;
+
+int main(int argc, char* argv[]) {
+
+	cout << "Hello, world!" << endl;
+	return;
 }
+
